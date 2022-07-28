988310ed1c4ecc9d28494fd9006fab2db1055ff9
priyarizal <103598087+priyarizal@users.noreply.github.com>
2022-07-28 09:52:52 -0400

    switch case & default for all options on the list

--- server.js
+++ server.js
@@ -40,12 +40,39 @@ function startInquirer () {
             name: 'startInquirer',
             message: 'What would you like to do?',
             choices: ['View all Departments', 'View all Roles' , 'View all Employees', 
-            'Add a department' , 'Add a role', 'Add an Employee', 'Update an Employee Role' ]
+            'Add a department' , 'Add a role', 'Add an Employee', 'Update an Employee Role', 'AllDone' ]
           }, 
         ])
             .then ((usersChoice) => {
                switch(usersChoice.startInquirer) {
-                case "Add a department"
+                case "View all Departments":
+                    viewDepartments()
+                    break;
+                case "View all Roles":
+                    viewRoles()
+                    break;
+                case "View all Employees":
+                    viewEmployees()
+                    break;
+                case "Add a department":
+                    addDepartment()
+                    break;
+                case "Add a role":
+                    addRoles()
+                    break;
+                case "Add an Employee":
+                    addEmployee()
+                    break;
+                case "Update an Employee Role":
+                    updateEmployeeRole()
+                    break;
+                case "All done":
+                    console.log ('-------------------');
+                    console.log('All Done');
+                    console.log ('--------------------');
+                    break;
+                default: 
+                console.log ("default")
                }
 
             })
